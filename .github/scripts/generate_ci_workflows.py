31d745969a1b7aee142f0a7cfd2a27a41a1f1e40
Ludovic Henry <git@ludovic.dev>
2025-06-07 12:50:04 +0000

    {wip} Add linux-riscv64 to CI

--- .github/scripts/generate_ci_workflows.py
+++ .github/scripts/generate_ci_workflows.py
@@ -102,6 +102,7 @@ class OperatingSystem:
     MACOS_ARM64 = "macos-arm64"
     LINUX_AARCH64 = "linux-aarch64"
     LINUX_S390X = "linux-s390x"
+    LINUX_RISCV64 = "linux-riscv64"
 
 
 LINUX_BINARY_BUILD_WORFKLOWS = [
@@ -365,6 +366,20 @@ S390X_BINARY_BUILD_WORKFLOWS = [
     ),
 ]
 
+RISCV64_BINARY_BUILD_WORKFLOWS = [
+    BinaryBuildWorkflow(
+        os=OperatingSystem.LINUX_RISCV64,
+        package_type="wheel",
+        build_configs=generate_binary_build_matrix.generate_wheels_matrix(
+            OperatingSystem.LINUX_RISCV64
+        ),
+        ciflow_config=CIFlowConfig(
+            labels={LABEL_CIFLOW_BINARIES, LABEL_CIFLOW_BINARIES_WHEEL},
+            isolated_workflow=True,
+        ),
+    ),
+]
+
 
 def main() -> None:
     jinja_env = jinja2.Environment(
@@ -386,6 +401,10 @@ def main() -> None:
         (
             jinja_env.get_template("linux_binary_build_workflow.yml.j2"),
             S390X_BINARY_BUILD_WORKFLOWS,
+        ),
+        (
+            jinja_env.get_template("linux_binary_build_workflow.yml.j2"),
+            RISCV64_BINARY_BUILD_WORKFLOWS,
         ),
         (
             jinja_env.get_template("linux_binary_build_workflow.yml.j2"),
